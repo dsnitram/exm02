022abeeee6998430337989302d5b19c9a61e6627
dsnitram <diegonwb@gmail.com>
2020-10-30 17:31:03 -0300

    Adiciona um controlador para tratar o home

---
+++ src/main/java/br/ufjf/dcc193/dsnitram/exm02/HomeController.java
@@ -0,0 +1,16 @@
+package br.ufjf.dcc193.dsnitram.exm02;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+  @RequestMapping({ "", "index.html" })
+  @ResponseBody
+  public String home(){
+    return "Hello World!";
+  }
+  
+}
