f8dd3d264a067d2bf0f8b76dd674c5f7c9e8adf2
dsnitram <diegonwb@gmail.com>
2020-10-30 15:46:34 -0300

    Adiciona umprojeto spring boot web inicial

---
+++ src/test/java/br/ufjf/dcc193/dsnitram/exm02/Exm02ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufjf.dcc193.dsnitram.exm02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exm02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
